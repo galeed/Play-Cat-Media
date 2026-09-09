50f8f4ef4d4909e39b784504d1a75d410c18fcdf
galeedx <113880595+galeed@users.noreply.github.com>
2026-09-09 14:19:36 -0600

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
 self.addEventListener('fetch', (event) => {
-  // Evitar que el Service Worker interfiera con blobs locales de música o peticiones parciales de audio
-  if (event.request.url.startsWith('blob:') || event.request.headers.get('range')) {
-    return; // Deja que el navegador maneje el archivo local de forma nativa
-  }
 
-  // Tu lógica actual de caché para index.html, estilos y temas va aquí abajo...
+  if (event.request.url.startsWith('blob:') || event.request.headers.get('range')) {
+    return; 
 });
 
 
@@ -14,7 +11,6 @@ const ASSETS = [
   'index.html'
 ];
 
-// Instalar y guardar el HTML único
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
@@ -22,7 +18,7 @@ self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
-// Servir el HTML desde la caché si no hay internet
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
